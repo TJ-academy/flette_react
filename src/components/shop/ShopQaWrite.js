ecdf8fc61d8c824b6eb3da696bce4dde9e368be8
guykim <life4guy@gmail.com>
2025-08-19 10:20:13 +0900

    i hate css

--- src/components/shop/ShopQaWrite.js
+++ src/components/shop/ShopQaWrite.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {useState} from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import '../../css/shop/shopqawrite.css';
 
 const ShopQaWrite = ({ onCancel, onSubmitSuccess }) => {
     const { productId } = useParams();
